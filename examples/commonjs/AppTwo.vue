42eda2ec118521366abeff6992fe2dbecd104eab
Barnabas Asha <barnabee58@gmail.com>
2023-03-16 18:43:31 +0100

    Updated ReadME to include using the library via provided composable

---
+++ examples/commonjs/AppTwo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="App">
+    <p class="App-intro">
+      <button @click="handlePayWithPaystack">Pay with paystack</button>
+    </p>
+  </div>
+</template>
+
+<script type="text/javascript">
+import usePaystackPayment from "../../src";
+
+export default {
+  setup() {
+    const paystackArgs = {
+      paystackkey: "pk_test_a137d402b5975716e89952a898aad2832c961d69",
+      email: "foobar@example.com",
+      amount: 1000000,
+      reference: "PayXYZ344234",
+    };
+
+    const payWithPaystack = usePaystackPayment(paystackArgs);
+
+    const callback = (response) => {
+      console.log(response);
+    };
+
+    const close = () => {
+      console.log("Payment closed");
+    };
+
+    const handlePayWithPaystack = () => {
+      payWithPaystack(callback, close);
+    };
+
+    return {
+      handlePayWithPaystack,
+    };
+  },
+};
+</script>
+<style>
+.App {
+  text-align: center;
+}
+
+.App-intro {
+  font-size: large;
+}
+.payButton {
+  color: #61dafb;
+  width: 250px;
+  height: 50px;
+  font-weight: 800;
+}
+</style>
